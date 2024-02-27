24890a5ee2fcdc90c80a3d1c9ad077442261228d
Sadegh Saedi Nia <sadegh@schwabeo.de>
2024-02-27 13:22:10 +0100

    deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/deploy-next",
+  //   basePath: "/deploy-next",
   output: "export",
-  reactStrictMode: true,
+  //   reactStrictMode: true,
 };
 
 export default nextConfig;
