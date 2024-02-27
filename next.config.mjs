db277f9022be915bb1b89cc3fdc607eda5f2ddf5
Sadegh Saedi Nia <sadegh@schwabeo.de>
2024-02-27 12:21:55 +0100

    Fixed deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: "/deploy-next",
+};
 
 export default nextConfig;
